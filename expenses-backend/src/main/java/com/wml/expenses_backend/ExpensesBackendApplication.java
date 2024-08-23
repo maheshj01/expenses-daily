826d8e6f195a92540cd40e8e428be368b5ec47ab
mahesh jamdade <maheshmn121@gmail.com>
2024-08-22 22:56:28 -0400

    disable security config

--- expenses-backend/src/main/java/com/wml/expenses_backend/ExpensesBackendApplication.java
+++ expenses-backend/src/main/java/com/wml/expenses_backend/ExpensesBackendApplication.java
@@ -2,11 +2,11 @@ package com.wml.expenses_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class ExpensesBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ExpensesBackendApplication.class, args);
 	}
-
 }
